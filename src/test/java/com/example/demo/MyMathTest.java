70d7f102d46b4d23ef8780f031480f95d56a7e7c
khadim NDAO <khadimndao5991@gmail.com>
2022-04-15 13:03:48 +0000

    correct error

--- src/test/java/com/example/demo/MyMathTest.java
+++ src/test/java/com/example/demo/MyMathTest.java
@@ -37,7 +37,7 @@ class MyMathTest {
     @Test
     public void sum_with3numbers() {
         System.out.println("Test1");
-        assertEquals(7, myMath.sum(new int[] { 1, 2, 3 }));
+        assertEquals(6, myMath.sum(new int[] { 1, 2, 3 }));
     }
 
     @Test
